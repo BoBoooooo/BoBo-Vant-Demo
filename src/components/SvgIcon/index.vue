5807444a16cfdf7d9b87cf2a9ee08a55e13b25f7
BoBoooooo <17746714@qq.com>
2020-09-10 09:37:35 +0800

    feat(svgIcon): 新增svgIcon

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,48 @@
+<!--
+ * @file: svgIcon
+ * @copyright: NanJing Anshare Tech .Com
+ * @author: BoBo
+ * @Date: 2020年09月10 09:21:28
+-->
+<template>
+  <svg :class="svgClass"
+       aria-hidden="true"
+       v-on="$listeners">
+    <use :xlink:href="iconName" />
+  </svg>
+</template>
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconClass: {
+      type: String,
+      required: true,
+    },
+    className: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    iconName() {
+      return `#icon-${this.iconClass}`;
+    },
+    svgClass() {
+      if (this.className) {
+        return `svg-icon ${this.className}`;
+      }
+      return 'svg-icon';
+    },
+  },
+};
+</script>
+<style scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
